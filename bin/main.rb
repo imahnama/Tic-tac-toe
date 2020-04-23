ee45cd632d5b6dcbde5e47474ea24ab50455515b
imahnama <muannama25@gmail.com>
2020-04-23 16:42:42 +0300

    Add classes, methods and validate moves of players

--- bin/main.rb
+++ bin/main.rb
@@ -2,55 +2,157 @@
 # Welcome user to the game
 puts 'Welcome to Tic Tac Toe!'
 
-user_name = 'Player_one'
-user_name1 = 'Player_two'
+class Board
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
 
-# prompt user to choose
-puts 'Kindly choose one: X or O?'
-player = gets.chomp.downcase
+  def display_board
+     puts (
+       "\n#{@board[0]} | #{@board[1]} | #{@board[2]}"\
+       "\n---------"\
+       "\n#{@board[3]} | #{@board[4]} | #{@board[5]}"\
+       "\n---------"\
+       "\n#{@board[6]} | #{@board[7]} | #{@board[8]} "
+       )
+  end
 
-if player == 'x'
-  puts "Welcome on board! You are #{user_name}"
-elsif player == 'o'
-  puts "Welcome on board! You are #{user_name1}"
+  def board_update(choice, player)
+    choice -= 1
+    @board[choice] = (player == 1 ? 'x' : 'o')
+  end
+
 end
 
-# diplay board on terminal
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+class Instructions < Board
+  def player_choice? (choice)
+    choice.between?(1,9)
+  end
 
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts '-----------'
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts '-----------'
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  def pos_taken?(choice)
+    @board[choice - 1] == 'x' || @board[choice - 1] == 'o'
+  end
 
-# player_one chooses position on the board
-puts "#{user_name} kindly choose a number between 1-9"
-player_one_choice = gets.chomp.to_i
-board[player_one_choice - 1] = player
+  def win?
+    "\nPlayer #{player} is our Tic Tac Toe Master!"
+  end
 
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts '-----------'
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts '-----------'
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  def draw
+    "\n Its a draw!"
+  end
+end
 
-puts 'Your move has been displayed on board!'
+game = Instructions.new
+game.display_board
 
-# player_two chooses position on the board
-puts "#{user_name1}, its now your turn. kindly choose a number between 1-9"
-player_one_choice = gets.chomp.to_i
-board[player_one_choice - 1] = player
+count = 0
+while count <= 1
+  (1..2).each do |i|
+    player = i
 
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts '-----------'
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts '-----------'
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    loop do
+      puts("\nPlayer #{i} It's your turn\n(choose between 1-9):")
+      choice = STDIN.gets.chomp.to_i
 
-puts 'Your move has been displayed on board!'
+      if game.player_choice?(choice)
+        if !game.pos_taken?(choice)
+          game.board_update(choice,player)
+          game.display_board
+          break
+        else
+          puts "\nPostion already taken, make another move"
+        end
+      end
+    end
+  end
 
-# display messages on different wins
-puts "Our Tic tac toe Master is #{user_name}!"
-puts "Our Tic tac toe Master is #{user_name1}!"
-puts 'It is a Draw!'
+  puts "end of round #{count}"
+  count += 1
+end
+
+puts 'Congratulations Player_one You are a Tic Tac Toe Master!'
+
+
+# # prompt user to choose
+# puts 'Kindly choose one: X or O?'
+# chip = gets.chomp.downcase
+# until %w[O X].include?(chip_1)
+#   puts "\nEnter a valid symbol"
+# chip_1 = gets.chomp
+# end
+#
+# chip_2 = board.get_chip(chip)
+#
+#
+#
+#
+#
+# if user == player_one
+#   puts "Welcome on board! Your chip is x"
+# elsif player == 'player_two'
+#   puts "Welcome on board! You chip is o"
+# else
+#   puts "Kindly enter a valid choice"
+# end
+#
+#
+# class Board
+# # diplay board on terminal
+# board =
+#
+#
+#   display_board(board)
+#
+# def move(board, index, player)
+#    board[index] = player
+# end
+#
+# def wins
+#   [[board[0]][1][2], [3][4][5], [6][7][8],
+#              [0][4][8], [2][4][6],  [0][3][6],
+#              [1][4][7], [2][5][8]]
+# end
+#
+# def get_chip(chip)
+#     chip == 'X' ? 'O' : 'X'
+# end
+#
+# end
+#
+# count = 0
+# game_on = true
+#
+# current_player = player_one.to_i
+# while game_on
+#
+# puts "Player_one kindly choose a number between 1-9"  # player_one chooses position on the board
+# player_choice = gets.chomp.to_i
+#
+# if player_choice  >0 && player_choice <9
+#
+# board[player_choice - 1] = player_one
+# puts display_board(board)
+# puts 'Your move has been displayed on board!'
+#
+# else
+#   puts "Enter a Valid move"
+# end
+#
+# puts "Player_two, its now your turn. kindly choose a number between 1-9" # player_two chooses position on the board
+# player_choice = gets.chomp.to_i
+# board[player_choice - 1] = player_two
+# puts display_board(board)
+# puts 'Your move has been displayed on board!'
+#
+# if  current_player.wins?()
+#   game_on = false
+#   puts "#{current_player} won!"
+#   break
+# end
+# count += 1
+#   current_player = current_player == player_one ? player_two : player_one
+# end
+# # display messages on different wins
+# puts "Our Tic tac toe Master is Player_one!"
+# puts "Our Tic tac toe Master is Player_two!"
+# puts 'It is a Draw!'
