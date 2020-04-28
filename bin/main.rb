cfda22806da27044518d641cbfc9c004a7ac5891
imahnama <muannama25@gmail.com>
2020-04-28 14:05:48 +0300

    add game flow rules

--- bin/main.rb
+++ bin/main.rb
@@ -3,60 +3,52 @@ require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 require_relative '../lib/board.rb'
 
-puts 'Welcome to Tic Tac Toe!'
-puts 'Choose between x and o'
-chip = gets.chomp.downcase
-
-if chip == 'x'
-  puts 'Welcome player_one'
-else chip == 'o'
-  puts 'welcome player_two'
+puts "Welcome to tic-tac-toe game!\n\n"
+board = Board.new
+puts 'Enter Player-1 name:'
+name1 = gets.chomp
+until /\S/.match(name1)
+  puts 'Enter Valid name'
+  name1 = gets.chomp
 end
-
-game = Board.new
-game.display_board
-
+puts "\nPlease select X or O"
+chip1 = gets.chomp.downcase
+until %w[o x].include?(chip1)
+  puts "\nEnter a valid symbol"
+  chip1 = gets.chomp.downcase
+end
+puts "\nEnter Player-2 name:"
+name2 = gets.chomp
+until /\S/.match(name2)
+  puts 'Enter Valid name'
+  name2 = gets.chomp
+end
+chip2 = board.get_chip(chip1)
+player_one = Player.new(name1, chip1)
+player_two = Player.new(name2, chip2)
+puts "\nHello #{name1}, your symbol is #{chip1}!"
+puts "\nHello #{name2}, your symbol is #{chip2}!\n\n"
+count = 0
+puts board.display_board
 game_on = true
-current_player = 'player_one'
+current_player = player_one
 while game_on
-  puts("\n #{current_player} It's your turn\n(choose between 1-9):")
-  choice = gets.chomp.to_i
-  until .player_choice?(choice)
-    puts 'Enter a valid choice'
-    choice = gets.chomp.to_i
-    game.display_board
+  puts "#{current_player.name} select a Position between (1-9)(#{current_player.chip})"
+  player_pos = gets.chomp.to_i
+  until board.position_taken?(player_pos)
+    puts "Enter a valid move (#{current_player.chip})\n\n"
+    puts board.display_board
+    player_pos = gets.chomp.to_i
   end
-  game.pos_taken?(choice)
-  game.board_update(choice, current_player)
-  game.display_board
-  
-  # %w[one two].each do |i|
-  #   player = i
-  #   loop do
-  #     puts("\nPlayer_#{i} It's your turn\n(choose between 1-9):")
-  #     choice = gets.chomp.to_i
-  #     if game.player_choice?(choice)
-  #       unless game.pos_taken?(choice)
-  #         game.board_update(choice, player)
-  #         game.display_board
-  #         break
-    
-  #         if player_one.wins > player_two.wins
-  #           puts 'Player_one wins!'
-  #         elsif player_two.wins > player_one.wins
-  #           puts 'Player_two wins!'
-  #         else
-  #           puts 'You tied!'
-  #         end
-  #         game_on = false
-  #       end
-  #       puts "\nIt's a win, comngratulations player_one!"
-  #     else
-  #       puts "\nPostion already taken, make another move"
-  #     end
-  #   end
-  # end
-  # puts "end of round #{count}"
-  # count += 1
+  # puts board.board_update(player_pos, current_player.chip)
+  puts board.board_update(player_pos, current_player.chip)
+  puts board.display_board
+  if current_player.wins?(player_pos)
+    game_on = false
+    puts "\n#{current_player.name} won!"
+    break
+  end
+  count += 1
+    current_player = current_player == player_one ? player_two : player_one
+  return puts "\nMatch Draw!!" if count == 9
 end
-puts 'Congratulations Player_one You are a Tic Tac Toe Master!'
