767ae4b329be8fec66634bb06dc83efba82d27ad
imahnama <muannama25@gmail.com>
2020-04-29 21:19:31 +0300

    call switch_player method and called current_player attr from board.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 
 puts 'Welcome to tic-tac-toe game!'
-board = Board.new
+player_chip = Player.new
 puts 'Enter Player_one name:'
 name1 = gets.chomp
 until /[a-z]/.match(name1)
@@ -22,32 +22,52 @@ until /[a-z]/.match(name2)
   puts 'Enter Valid name'
   name2 = gets.chomp
 end
-chip2 = board.get_chip(chip1)
+chip2 = player_chip.get_chip(chip1)
 player_one = Player.new(name1, chip1)
 player_two = Player.new(name2, chip2)
 puts "Hello #{name1}, your chip is #{chip1}!"
 puts "Hello #{name2}, your chip is #{chip2}!"
 count = 0
+
+current_player = player_one
+board = Board.new(current_player, player_one, player_two)
 puts board.display_board
 game_on = true
-current_player = player_one
 while game_on
-  puts "#{current_player.name} select a Position between (1-9)"
+  puts "#{board.current_player.name} select a Position between (1-9)"
   player_pos = gets.chomp.to_i
   until board.position_taken?(player_pos)
     puts 'Enter a valid position between 1-9:'
     puts board.display_board
     player_pos = gets.chomp.to_i
   end
-
-  board.board_update(player_pos, current_player.chip)
+  board.board_update(player_pos)
   puts board.display_board
   if board.wins?(player_pos)
     game_on = false
-    puts "Congratulations#{ current_player.name} You are a Tic Tac Toe Master!"
+    puts "Congratulations #{board.current_player.name} You are a Tic Tac Toe Master!"
     break
   end
   count += 1
- board.switch_player(current_player)
-  return puts "It's a Draw!!" if count == 9
+  board.switch_player # (current_player)
 end
+# while game_on
+#   puts "#{current_player.name} select a Position between (1-9)"
+#   player_pos = gets.chomp.to_i
+#   until board.position_taken?(player_pos)
+#     puts 'Enter a valid position between 1-9:'
+#     puts board.display_board
+#     player_pos = gets.chomp.to_i
+#   end
+#
+#   board.board_update(player_pos, current_player.chip)
+#   puts board.display_board
+#   if board.wins?(player_pos)
+#     game_on = false
+#     puts "Congratulations#{ current_player.name} You are a Tic Tac Toe Master!"
+#     break
+#   end
+#   count += 1
+#  board.switch_player(current_player)
+#   return puts "It's a Draw!!" if count == 9
+# end
