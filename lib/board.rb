e7a16232f22e523ffd53496313a8395e5fe4d416
imahnama <muannama25@gmail.com>
2020-04-29 11:53:56 +0300

    add method switch player and initialize players

--- lib/board.rb
+++ lib/board.rb
@@ -1,26 +1,47 @@
 class Board
-  attr_reader :board
-  def initialize
+
+ attr_reader :current_player, :other_player
+  def initialize(current_player= nil, player_one = nil, player_two=nil)
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @final_array = []
+    @player_one = player_one
+    @player_two = player_two
+    @win_arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                [1, 5, 9], [3, 5, 7]]
   end
 
   def display_board(_choice = nil, _current_player = nil)
-    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts '---------'
-    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts '---------'
-    puts "#{@board[6]} | #{@board[7]} | #{@board[8]} "
+    "#{@board[0]} | #{@board[1]} | #{@board[2]}
+---------
+#{@board[3]} | #{@board[4]} | #{@board[5]}
+---------
+#{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def get_chip(chip)
     chip == 'x' ? 'o' : 'x'
   end
 
-  def board_update(_board, choice, player)
+  def board_update( choice, player)
     @board[choice - 1] = player
   end
 
   def position_taken?(choice)
     @board[choice - 1] == 'x' || @board[choice - 1] == 'o' || choice > 9 || choice < 1 ? false : true
   end
+
+  def wins?(choice)
+    @final_array.push(choice)
+    @winner = false
+    @win_arr.each do |arr|
+      @winner = true if @final_array.include?(arr[0]) && @final_array.include?(arr[1]) && @final_array.include?(arr[2])
+    end
+    @winner
+  end
+
+  def switch_player
+    current_player = current_player == @player_one ? @player_two : @player_one
+end
+
 end
