60e7af1bcd559ca6cf20ebd44b75788ecb9c1acd
imahnama <muannama25@gmail.com>
2020-04-29 21:18:02 +0300

    Changed Win_arr to a constant variable and added all instances of players as attr_readers

--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,26 @@
 class Board
+attr_reader :current_player, :player_one, :player_two
 
- attr_reader
+WIN_ARR = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+            [1, 5, 9], [3, 5, 7]]
+
   def initialize(current_player= nil, player_one = nil, player_two=nil)
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @final_array = []
     @current_player = current_player
     @player_one = player_one
     @player_two = player_two
-    @win_arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-                [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                [1, 5, 9], [3, 5, 7]]
+    @final_array = []
+
   end
 
   def display_board(_choice = nil, _current_player = nil)
-    "#{@board[0]} | #{@board[1]} | #{@board[2]}
----------
-#{@board[3]} | #{@board[4]} | #{@board[5]}
----------
-#{@board[6]} | #{@board[7]} | #{@board[8]} "
+    "#{@board[0]} | #{@board[1]} | #{@board[2]}\n---------\n#{@board[3]} | #{@board[4]} | #{@board[5]}\n---------\n#{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def get_chip(chip)
-    chip == 'x' ? 'o' : 'x'
-  end
+  def board_update( choice)
 
-  def board_update( choice, player)
-    @board[choice - 1] = player
+    @board[choice - 1] = @current_player.chip
   end
 
   def position_taken?(choice)
@@ -33,15 +28,16 @@ class Board
   end
 
   def wins?(choice)
+
     @final_array.push(choice)
     @winner = false
-    @win_arr.each do |arr|
+    WIN_ARR.each do |arr|
       @winner = true if @final_array.include?(arr[0]) && @final_array.include?(arr[1]) && @final_array.include?(arr[2])
     end
     @winner
   end
 
-  def switch_player(current_player)
+  def switch_player
     @current_player = @current_player == @player_one ? @player_two : @player_one
 end
 
