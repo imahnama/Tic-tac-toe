b86eed9830f2904aec966dec77e0c81d3ed04b14
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-05-04 20:44:05 +0300

    fix linter errors

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,13 @@
 class Player
-    attr_reader :name, :chip, :moves_history
-    attr_writer :moves_history
-    def initialize(name = nil, chip = nil)
-      @name = name
-      @chip = chip
-      @moves_history = []
-    end
-  
-    def get_chip(chip)
-      chip == 'x' ? 'o' : 'x'
-    end
+  attr_reader :name, :chip, :moves_history
+  attr_writer :moves_history
+  def initialize(name = nil, chip = nil)
+    @name = name
+    @chip = chip
+    @moves_history = []
+  end
+
+  def get_chip(chip)
+    chip == 'x' ? 'o' : 'x'
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,20 +1,19 @@
 require './lib/board.rb'
 
 describe Board do
-  let(:board) {Board.new}
+  let(:board) { Board.new }
   let(:player_one) { Board.new }
   let(:player_two) { Board.new }
   let(:board) { Board.new(:player_one, :player_two, :board) }
   let(:current_player) { :player_one }
-
 
   describe '#initialize' do
     it 'should initialize as Board class' do
       expect(board.class).to eql(Board)
     end
 
-    it "#initialize the board with a grid" do
-      expect {Board.new(board: "board")}.to_not raise_error
+    it '#initialize the board with a grid' do
+      expect { Board.new(board: 'board') }.to_not raise_error
     end
   end
 
@@ -25,7 +24,7 @@ describe Board do
     it 'returns false if a choice made is invalid' do
       expect(board.position_taken?(10)).not_to be true
     end
-  
+
     it 'returns error if no argument is given' do
       expect { board.position_taken? }.to raise_error(ArgumentError)
     end
@@ -35,30 +34,28 @@ describe Board do
     it 'returns the grid of string if correct position is passed' do
       expect(board.display_board(3, 'x')).to be_instance_of(String)
     end
-        
   end
   describe '#wins?' do
-   
     it 'returns false if the player doesnt win the game' do
-      expect(board.wins?(['1', '3', '5'])).to be false
+      expect(board.wins?(%w[1 3 5])).to be false
     end
     it 'returns false if the player doesnt win the game' do
       expect(board.wins?('2')).to be false
     end
     it 'returns true if player wins the game' do
-      choice = ['1', '2', '3']
+      choice = %w[1 2 3]
       actual = board.wins?(choice)
       expect(actual).to be true
     end
     it 'returns error if no argument is passed' do
-      expect {board.wins? }.to raise_error(ArgumentError)
+      expect { board.wins? }.to raise_error(ArgumentError)
     end
   end
 
   describe '#board_update' do
     chip = 'x'
     choice = '7'
-    it 'replaces choice made with current player chip' do   
+    it 'replaces choice made with current player chip' do
       actual = board.display_board[choice] == chip
       expected = board.display_board[7] == 'x'
       expect(actual).to eq expected
